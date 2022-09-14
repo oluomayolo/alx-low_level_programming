79f30b63f6e081c897f6243d1b5c9858cabb58e6
oluomayolo <oluomaokechukwu2020@gmail.com>
2022-09-13 17:04:36 -0700

    task 1 nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
 	int i;
